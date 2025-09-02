c998f8f9f38e202c03e7ec78bf51d91785e6183e
JO <ruxxer30@gmail.com>
2025-09-02 17:35:10 +0900

    사분면 고르기

---
+++ BOJ 2-4단계 14681.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+
+if x == x > 0 and y == y > 0:
+    print(1)
+if x == x < 0 and y == y > 0:
+    print(2)
+if x == x < 0 and y == y < 0:
+    print(3)
+if x == x > 0 and y == y < 0:
+    print(4)
